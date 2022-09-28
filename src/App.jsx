eb2e15cf0fc71e2da32c20ae06276727191758a5
jo4ovictor <joaovictordeaguiarmoura@gmail.com>
2022-09-28 09:00:52 -0400

    modificando arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import Menu from "./Menu"
-import Card from "./Card";
-import "./App.css"
-function App(){
-	const sites = [
-		{	
-			foto: "big-king.webp",
-			nome: "Combo Big King",
-			des: "Um Hambúrguer com duas carnes bovinas grelhadas de 57g. pão com gergelin, queijo...",
-			qtd: "Serve 1 pessoa",
-			preco: "R$ 41,90"
-		},
-	];
-	return(
-		
-		<div className="conteiner">
-			<Menu/>
-			<div className="tit">Destaques</div>
-			<div className="produto">
-			{
-				sites.map((site) => {
-					return (
-					<Card  foto={site.foto}
-							nome={site.nome}
-							des={site.des}
-							qtd={site.qtd}
-							preco={site.preco}
-						/>
-					)
-				})
+import RoutesApp from "./routes";
 
-			
-			}
-			</div>
+function App(){
+	
+	return(
+		<div className="App">
+			<h2>Teste</h2>
+		<RoutesApp/>
 		</div>
-		
 	);
 }
 
